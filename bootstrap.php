bc3fa2ce07d47e91359cb4c6dc373a56c66cce75
Dan Morrison <dan@coders.co.nz>
2015-04-15 05:50:15 +1200

    Make bootstrap.php aware of the vendor directory

--- bootstrap.php
+++ bootstrap.php
@@ -10,7 +10,10 @@
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 
-require_once "vendor/autoload.php";
+#require_once "vendor/autoload.php";
+// May be called as a to level project,
+// or after being placed into the vendor directory myself.
+(@include_once __DIR__ . '/vendor/autoload.php') || @include_once __DIR__ . '/../../autoload.php';
 
 // Create a simple "default" Doctrine ORM configuration for Annotations.
 $is_dev_mode = TRUE;
